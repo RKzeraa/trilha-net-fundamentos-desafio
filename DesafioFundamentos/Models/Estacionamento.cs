8c4d94eb9d71557207fe881e6ef94f423fd3ca9a
Ramom Karllos <rkzeraa@gmail.com>
2023-12-09 05:34:17 -0300

    [Feat] - Was the 'Remover Veículo' logic implemented and Refactor 'Estacionamento'

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -5,46 +5,47 @@ namespace DesafioFundamentos.Models
 {
     public class Estacionamento
     {
-        private decimal precoInicial = 0;
-        private decimal precoPorHora = 0;
-        private List<string> veiculos = new List<string>();
+        private decimal _precoInicial = 0;
+        private decimal _precoPorHora = 0;
+        private List<Tuple<string, DateTime>> _veiculos = new List<Tuple<string, DateTime>>();
 
-        private readonly TextCustom textCustom = new TextCustom();
+        private readonly TextCustom _textCustom = new TextCustom();
 
         public Estacionamento(decimal precoInicial, decimal precoPorHora)
         {
-            this.precoInicial = precoInicial;
-            this.precoPorHora = precoPorHora;
+            _precoInicial = precoInicial;
+            _precoPorHora = precoPorHora;
         }
 
         public void AdicionarVeiculo()
         {
             // TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
             // *IMPLEMENTE AQUI*
-            textCustom.ShowLogo();
+            _textCustom.ShowLogo();
             Console.WriteLine("Digite a placa do veículo para estacionar:");
             string placa = Console.ReadLine().Trim().Replace("-", "").ToUpper();
             while(true){
                 if(!ExistePlaca(placa))
                 {
                     if(PlacaValida(placa)){
-                        textCustom.ShowLogo();
-                        veiculos.Add(placa);
-                        textCustom.ApplyColor($"O veículo {FormatarPlaca(placa)} foi liberado para estacionar! ✅", ConsoleColor.DarkGreen);
+                        _textCustom.ShowLogo();
+                        DateTime horaEntrada = DateTime.Now;
+                        _veiculos.Add(new (placa, horaEntrada));
+                        _textCustom.ApplyColor($"O veículo {FormatarPlaca(placa)} foi liberado para estacionar! ✅ \nHora de Entrada: {horaEntrada.ToString("HH:mm")}", ConsoleColor.DarkGreen);
                         break;
                     }
                     else
                     {
-                        textCustom.ShowLogo();
-                        textCustom.ApplyColor("Desculpe, essa placa não é válida. ❌\nExemplo de placa valida: AAA-0000 ou AAA0000 ou AAA0A00 ⚠", ConsoleColor.Red);
+                        _textCustom.ShowLogo();
+                        _textCustom.ApplyColor("Desculpe, essa placa não é válida. ❌\nExemplo de placa valida: AAA-0000 ou AAA0000 ou AAA0A00 ⚠", ConsoleColor.Red);
                         Console.WriteLine("Digite uma placa válida:");
                         placa = Console.ReadLine().Trim().Replace("-", "").ToUpper();
                     }
                 }
                 else
                 {
-                    textCustom.ShowLogo();
-                    textCustom.ApplyColor($"Desculpe, esse veículo {FormatarPlaca(placa)} já está estacionado aqui. ❌" +
+                    _textCustom.ShowLogo();
+                    _textCustom.ApplyColor($"Desculpe, esse veículo {FormatarPlaca(placa)} já está estacionado aqui. ❌" +
                      "\nCaso a placa e as características dos veículos sejam idênticas, favor entrar em contato com a policia." +
                      "\nDisk Denuncia: 181", ConsoleColor.Red);
                     break;
@@ -54,7 +55,7 @@ namespace DesafioFundamentos.Models
 
         private bool ExistePlaca(string placa)
         {
-            if(veiculos.Any(x => x == placa))
+            if(_veiculos.Any(x => x.Item1 == placa))
             { 
                 return true;
             }
@@ -106,53 +107,89 @@ namespace DesafioFundamentos.Models
         }
         public void RemoverVeiculo()
         {
-            textCustom.ShowLogo();
-            Console.WriteLine("Digite a placa do veículo para remover:");
-
-            // Pedir para o usuário digitar a placa e armazenar na variável placa
-            // *IMPLEMENTE AQUI*
-            string placa = "";
-
-            // Verifica se o veículo existe
-            if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
+            if(!_veiculos.Any())
             {
-                textCustom.ShowLogo();
-                Console.WriteLine($"Digite a quantidade de horas que o veículo {placa} permaneceu estacionado:");
-
-                // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
-                // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal                
-                // *IMPLEMENTE AQUI*
-                int horas = 0;
-                decimal valorTotal = 0; 
-
-                // TODO: Remover a placa digitada da lista de veículos
-                // *IMPLEMENTE AQUI*
-
-                textCustom.ApplyColor($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal} ✔", ConsoleColor.DarkGreen);
-            }
+                _textCustom.ShowLogo();
+                _textCustom.ApplyColor("Não há veículos estacionados. ❌", ConsoleColor.Red);
+                return;
+            } 
             else
             {
-                textCustom.ApplyColor($"Desculpe, esse veículo {placa} não está estacionado aqui. ❌\nConfira se digitou a placa corretamente ⚠", ConsoleColor.Red);
+                _textCustom.ShowLogo();
+                ListarVeiculos();
+                Console.WriteLine("\nDigite a placa do veículo para remover:");
+                // Pedir para o usuário digitar a placa e armazenar na variável placa
+                // *IMPLEMENTE AQUI*
+                string placa = Console.ReadLine().Trim().Replace("-", "").ToUpper();
+                while(true){            
+                    // Verifica se o veículo existe
+                    if (ExistePlaca(placa))
+                    {
+                        DateTime horaEntrada = _veiculos.FirstOrDefault(x => x.Item1 == placa).Item2;
+                        DateTime horaSaida = DateTime.Now;
+                        int diferenca = (int)CalcularDiferencaDeHoras(horaEntrada, horaSaida).TotalHours;
+                        _textCustom.ShowLogo();
+                        Console.WriteLine($"O veículo {FormatarPlaca(placa)} permaneceu estacionado por {diferenca}h");
+                        // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal                
+                        // *IMPLEMENTE AQUI*
+
+                        decimal valorTotal = ValorPagar(diferenca);
+
+                        // TODO: Remover a placa digitada da lista de veículos
+                        _veiculos.Remove(_veiculos.FirstOrDefault(x => x.Item1 == placa));
+                        // *IMPLEMENTE AQUI*
+
+                        _textCustom.ApplyColor($"Saida liberada {FormatarPlaca(placa)}, o preço total pago foi de: R$ {valorTotal} ✔", ConsoleColor.DarkGreen);
+                        break;
+                    }
+                    else
+                    {
+                        if(!PlacaValida(placa))
+                        {
+                            _textCustom.ShowLogo();
+                            _textCustom.ApplyColor("Desculpe, essa placa não é válida. ❌\nExemplo de placa valida: AAA-0000 ou AAA0000 ou AAA0A00 ⚠", ConsoleColor.Red);
+                            Console.WriteLine("Digite uma placa válida:");
+                            placa = Console.ReadLine().Trim().Replace("-", "").ToUpper();
+                        }
+                        else
+                        {
+                            _textCustom.ShowLogo();
+                            _textCustom.ApplyColor($"Desculpe, esse veículo {FormatarPlaca(placa)} não está estacionado aqui. ❌\nConfira se digitou a placa corretamente ⚠", ConsoleColor.Red);
+                            break;
+                        }
+                    }
+                }
             }
+        }
+
+        private decimal ValorPagar(int diferenca)
+        {
+            return _precoInicial + _precoPorHora * diferenca;
+        }
+
+        private static TimeSpan CalcularDiferencaDeHoras(DateTime horaEntrada, DateTime horaSaida)
+        {
+            TimeSpan diferenca = horaSaida - horaEntrada;
+            return diferenca;
         }
 
         public void ListarVeiculos()
         {
             // Verifica se há veículos no estacionamento
-            if (veiculos.Any())
+            _textCustom.ShowLogo();
+            if (_veiculos.Any())
             {
-                textCustom.ShowLogo();
                 Console.WriteLine("Os veículos estacionados são:");
                 // TODO: Realizar um laço de repetição, exibindo os veículos estacionados
                 // *IMPLEMENTE AQUI*
-                foreach (var veiculo in veiculos)
+                foreach (var veiculo in _veiculos)
                 {
-                    textCustom.ApplyColor($"[{FormatarPlaca(veiculo)}]", ConsoleColor.DarkGreen);
+                    _textCustom.ApplyColor($"Placa: [{FormatarPlaca(veiculo.Item1)}] - Hora de Entrada: {veiculo.Item2.ToString("HH:mm")}", ConsoleColor.DarkGreen);
                 }
             }
             else
             {   
-                textCustom.ApplyColor("Não há veículos estacionados. ❌", ConsoleColor.Red);
+                _textCustom.ApplyColor("Não há veículos estacionados. ❌", ConsoleColor.Red);
             }
         }
     }
